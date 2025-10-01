d5c6afbf7bd1d1c4721aea605e115697b9303219
SimoL-003 <simoneleuzzi2003@gmail.com>
2025-10-01 14:49:19 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# Calcolo del prezzo del biglietto del treno
+
+## Italiano
+
+Scrivere un programma che chieda all'utente:
+
+- Il numero di chilometri da percorrere
+- Età del passeggero
+
+Sulla base di queste informazioni dovrà calcolare il prezzo totale del biglietto di viaggio, secondo le seguenti regole:
+
+- il prezzo del biglietto è definito in base ai km (0.21 € al km)
+- va applicato uno sconto del 20% per i minorenni
+- va applicato uno sconto del 40% per gli over 65
+
+## English
+
+Write a program that asks the user:
+
+- The number of kilometers to travel
+- The passenger's age
+
+Based on this information it should calculate the total ticket price, according to the following rules:
+
+- The ticket price is defined based on km (€0.21 per km)
+- A 20% discount applies for minors
+- A 40% discount applies for passengers over 65
---
+++ js/script.js
@@ -0,0 +1,32 @@
+// Prendere il form e gli input
+
+const formElem = document.querySelector("form");
+const nameInput = document.getElementById("name");
+const kmInput = document.getElementById("km");
+const ticketInput = document.getElementById("ticket");
+
+// Variabili per il calcolo del biglietto
+
+const priceKm = 0.21;
+const discountUnder18 = 0.2;
+const discountOver65 = 0.4;
+
+// All'invio del form
+
+formElem.addEventListener("submit", function (event) {
+    event.preventDefault(); /* Per non ricaricare la pagina */
+
+    const kmInputNum = parseInt(kmInput.value);
+
+    const standardPrice = priceKm * kmInputNum;
+    let finalPrice = standardPrice;
+
+    if (ticketInput.value === "over65") {
+        finalPrice = standardPrice - standardPrice * discountOver65;
+    } else if (ticketInput.value === "under18") {
+        finalPrice = standardPrice - standardPrice * discountUnder18;
+    }
+
+    let result = finalPrice;
+    console.log(result.toFixed(2));
+})
